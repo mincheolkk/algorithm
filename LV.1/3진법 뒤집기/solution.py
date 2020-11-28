b6f56936f0e7518e014782e29958cd9d8394161b
mincheolkk <33251241+mincheolkk@users.noreply.github.com>
2020-11-28 22:33:51 +0900

    Update solution.py

--- LV.1/3진법 뒤집기/solution.py
+++ LV.1/3진법 뒤집기/solution.py
@@ -1,3 +1,4 @@
+#solution 1
 def solution(m):
     n =3
     r = m%n
@@ -22,4 +23,29 @@ def solution(m):
     answer = 0
     for i in range(len(three)):
         answer += 3**(i)*int(three[i])
+    return answer
+
+
+#solution 2
+def solution(m):
+    n =3 
+    r = m%n
+    na = []
+    
+    if (m<=2):
+        return m
+    while (n>0):
+        m = (m//n)*n + r
+        if m<3:
+            break
+        na.append(r)
+        m = (m//n)
+        r = m%n
+        
+    na.append(r)
+    na.reverse()
+
+    answer = 0
+    for i in range(len(na)):
+        answer += 3**(i)*int(na[i])
     return answer
