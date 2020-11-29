e729cebbad829d0c05e4c33a0f309ec100e72826
mincheolkk <33251241+mincheolkk@users.noreply.github.com>
2020-11-29 23:35:05 +0900

    Update solution.py

--- LV.2/피보나치 수/solution.py
+++ LV.2/피보나치 수/solution.py
@@ -1,3 +1,19 @@
+#첫 풀이는 재귀, 이 문제의 시간조건에선 시간초과로 답으로 인정되지는 않았다.
+def solution(n):
+    def fibo(n):
+        if n ==0:
+            return 0
+        elif n == 1 :
+            return 1
+        else:
+            return fibo(n-1)+fibo(n-2)
+
+    answer = fibo(n)%1234567
+    return answer
+
+
+#두 번째 풀이는 for문 
+
 def solution(n):
     answer = 0
     final_minus_one = 0
