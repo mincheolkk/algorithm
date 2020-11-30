fe1da6efb5c2002d5fe7da4fc566ecb641732299
mincheolkk <33251241+mincheolkk@users.noreply.github.com>
2020-11-30 23:15:01 +0900

    Update solution.py

--- LV.1/완주하지 못한 선수/solution.py
+++ LV.1/완주하지 못한 선수/solution.py
@@ -5,3 +5,15 @@ def solution(participant, completion):
     c = collections.Counter(completion) 
 
     return list(p-c)[0]
+
+
+#첫풀이, 정확성은 다 맞았으나 효율성은 실패. 
+#for 문에 pop 연산까지 O(N^2) 
+def solution(participant, completion):
+    for i in completion:
+        if i in participant:    
+            b = participant.index(i)
+            participant.pop(b)  
+                  
+    answer = participant[0]
+    return answer
