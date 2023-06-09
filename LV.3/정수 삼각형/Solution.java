9400f158769dc9de2379f7952f49d18813151623
mincheolkk <33251241+mincheolkk@users.noreply.github.com>
2023-06-09 23:24:30 +0900

    Create Solution.java
    아래서 부터 위로 올라가면서 더해가기

---
+++ LV.3/정수 삼각형/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int height = triangle.length;
+
+        for (int i=height-2; i>=0; i--) {
+            for (int j=0; j < triangle[i].length; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+        
+        return triangle[0][0];
+    }
+}
